178f2332df2fc49377c19f7933d400ecda08f964
micodocious <46972164+micodocious@users.noreply.github.com>
2019-06-11 16:14:30 -0700

    Changed Variables

--- game_of_life.py
+++ game_of_life.py
@@ -1,19 +1,16 @@
 #! /usr/bin/env python3
 """
-   Conway's Game of Life
+Conway's Game of Life
 
-   Longer, more detailed explanation of your solution. This may be anywhere from
-   a couple of sentences to several paragraphs depending on how complex the
-   assignment is. It should be detailed enough that someone who has never seen the
-   code can understand what it does just by reading this explanation. You may
-   also want to include examples of using the program if it accepts command line
-   arguments.
+The square class is what we use to set up the grid for the program.
+The grid class is what we use to set up the rules and manipulate the squares.
+The app class is what we use to set up the program with TKinter.
 
-   Mico Santiago
+Mico Santiago
 """
 
 
-from tkinter import *
+import tkinter as tk
 import random
 
 
@@ -23,11 +20,11 @@ class Square:
     def __init__(
         self,
         coords,
-        length,
-        size,
-        state=False,
-        active_col="black",
-        inactive_col="white",
+        map_size,
+        side_length,
+        is_alive=False,
+        alive_col="black",
+        dead_col="white",
     ):
         """
         Initialization function.
@@ -36,27 +33,27 @@ class Square:
 
         Parameters
         ----------
-        length : int
-            Size of map
+        map_size : int
+            side_length of map
         coords : str
             Top left corner
-        size : int
-            Length of one side
-        state : boolean
+        side_length : int
+            map_size of one side
+        is_alive : boolean
             Alive or dead
-        active_colour  : str
+        alive_colour  : str
             Colour if alive
-        inactive_colour : str
+        dead_colour : str
             Colour if dead
 
         """
 
-        self.length = length
+        self.map_size = map_size
         self.coords = coords
-        self.size = size
-        self.state = state
-        self.active_colour = active_col
-        self.inactive_colour = inactive_col
+        self.side_length = side_length
+        self.is_alive = is_alive
+        self.alive_colour = alive_col
+        self.dead_colour = dead_col
 
     def rect(self):
         """
@@ -64,18 +61,20 @@ class Square:
 
         Returns
         -------
-        self.coords[0] + self.size, self.coords[1] + self.size
-            x+size, y+size
+        self.coords[0] + self.side_length, self.coords[1] + self.side_length
+            x+side_length, y+side_length
 
         """
-        return (self.coords[0] + self.size, self.coords[1] + self.size)
+        return (self.coords[0] + self.side_length, self.coords[1] + self.side_length)
 
     def inbounds(self, coord):
         """
-        Returns whether a coordinate is inbounds in the grid
+        Returns whether a coordinate is inbounds in the grid.
 
-        Checks if x value is >= 0 and if the right side of the square is not off the board as x value is top left.
-        Checks if y value is >= 0 and if the bottom side of the square is not off the board as y value is top left.
+        Checks if x value is >= 0 and if the right side of the square is not
+        off the board as x value is top left.
+        Checks if y value is >= 0 and if the bottom side of the square is not
+        off the board as y value is top left.
 
         Returns
         -------
@@ -83,10 +82,10 @@ class Square:
             True or false
 
         """
-        (x, y) = coord
+        (x_coor, y_coor) = coord
 
-        return (x >= 0 and x <= self.length - self.size) and (
-            y >= 0 and y <= self.length - self.size
+        return (0 <= x_coor <= self.map_size - self.side_length) and (
+            0 <= y_coor <= self.map_size - self.side_length
         )
 
     def neighbours(self):
@@ -95,9 +94,11 @@ class Square:
 
         self.coords is a tuple. Extracting the x and y of it
 
-        filter(func, iterable) loops over each value and keeps the value if the function called per value is true.
-        I convert back to list as filter object isn't easy to deal with in my program
-        Each item in the list is dictated by the current x or y +/- size.
+        filter(func, iterable) loops over each value and keeps the value if the
+        function called per value is true.
+        I convert back to list as filter object isn't easy to deal with in my
+        program.
+        Each item in the list is dictated by the current x or y +/- side_length.
 
         Returns
         -------
@@ -105,20 +106,20 @@ class Square:
             Returns a colour whether the object is alive or dead
 
         """
-        (x, y) = self.coords
+        (x_coord, y_coord) = self.coords
 
         return list(
             filter(
                 self.inbounds,
                 [
-                    (x - self.size, y + self.size),
-                    (x, y + self.size),
-                    (x + self.size, y + self.size),
-                    (x - self.size, y),
-                    (x + self.size, y),
-                    (x - self.size, y - self.size),
-                    (x, y - self.size),
-                    (x + self.size, y - self.size),
+                    (x_coord - self.side_length, y_coord + self.side_length),
+                    (x_coord, y_coord + self.side_length),
+                    (x_coord + self.side_length, y_coord + self.side_length),
+                    (x_coord - self.side_length, y_coord),
+                    (x_coord + self.side_length, y_coord),
+                    (x_coord - self.side_length, y_coord - self.side_length),
+                    (x_coord, y_coord - self.side_length),
+                    (x_coord + self.side_length, y_coord - self.side_length),
                 ],
             )
         )
@@ -127,7 +128,7 @@ class Square:
         """
         Returns a colour whether the object is alive or dead
 
-        Short hand if statement
+        Short hand if is_alivement
         If object is alive return alive colour
         Or else (only two options possible) return dead colour
 
@@ -137,14 +138,14 @@ class Square:
             If object is alive return alive colour or else return dead colour
 
         """
-        return self.active_colour if self.state else self.inactive_colour
+        return self.alive_colour if self.is_alive else self.dead_colour
 
 
 class Grid:
     """Grid class: The map of each square"""
 
     def __init__(
-        self, length, size, tolerance, active_col="black", inactive_col="white"
+        self, map_size, side_length, tolerance, alive_col="black", dead_col="white"
     ):
         """
         Initialization function.
@@ -153,33 +154,36 @@ class Grid:
 
         Parameters
         ----------
-        length : int
-            The length of the map
-        size : int
-            Length of one side
+        map_size : int
+            The map_size of the map
+        side_length : int
+            map_size of one side
         tolerance : float
             The tolerance of generating alive cells randomly
-        active_col  : str
+        alive_col  : str
             Colour if alive
-        inactive_col : str
+        dead_col : str
             Colour if dead
 
         """
 
-        self.length = length
+        self.map_size = map_size
         self.tolerance = tolerance
-        self.active_col = active_col
-        self.inactive_col = inactive_col
+        self.alive_col = alive_col
+        self.dead_col = dead_col
 
-        self.squares = self.make_squares(size)
+        self.squares = self.make_squares(side_length)
 
-    def make_squares(self, size):
+    def make_squares(self, side_length):
         """
         Creates a dictionary of square objects
 
-        Row loop through the 'length' in steps of 'size' (so as to get the right top left corner each time),
-        then cells loop through the 'length' in steps of 'size' (so as to get the right top left corner each time).
-        If the random float is less than tolerance then make it start dead. Otherwise make it alive.
+        Row loop through the 'map_size' in steps of 'side_length'
+        (so as to get the right top left corner each time),
+        then cells loop through the 'map_size' in steps of 'side_length'
+        (so as to get the right top left corner each time).
+        If the random float is less than tolerance then make it start dead.
+        Otherwise make it alive.
 
         Returns
         -------
@@ -187,24 +191,24 @@ class Grid:
             Returns a dictionary of squares.
         """
         squares = {}
-        for y in range(0, self.length, size):
-            for x in range(0, self.length, size):
+        for y in range(0, self.map_size, side_length):
+            for x in range(0, self.map_size, side_length):
                 if random.random() < self.tolerance:
                     squares[(x, y)] = Square(
                         (x, y),
-                        self.length,
-                        size,
-                        active_col=self.active_col,
-                        inactive_col=self.inactive_col,
+                        self.map_size,
+                        side_length,
+                        alive_col=self.alive_col,
+                        dead_col=self.dead_col,
                     )
                 else:
                     squares[(x, y)] = Square(
                         (x, y),
-                        self.length,
-                        size,
-                        state=True,
-                        active_col=self.active_col,
-                        inactive_col=self.inactive_col,
+                        self.map_size,
+                        side_length,
+                        is_alive=True,
+                        alive_col=self.alive_col,
+                        dead_col=self.dead_col,
                     )
         return squares
 
@@ -224,112 +228,109 @@ class Grid:
         """
         for coord, square in self.squares:
             if coord in on_coordinates:
-                square.state = True
+                square.is_alive = True
 
     def rules(self):
         """
-        A set of rules , as defined at the top of this script, to be applied to the grid
+        A set of rules , as defined at the top of this script, to be applied to
+        the grid.
 
         Looping through each square
-            Create a variable to keep track of alive neighbours. Refreshes each square
+            Create a variable to keep track of alive neighbours and
+            refreshes each square.
             Grab all the squares neighbours
             Loop through each neighbour
                 If the neighbour is alive:
-                    Increment the counter of alive neighbours
+                    Increment the counter of alive neighbours.
             If the square is alive:
                 RULE 1:
-                    Kill the square
+                    Kill the square.
                 RULE 2:
-                    Keep it alive
+                    Keep it alive.
                 RULE 3:
-                    Kill the square
+                    Kill the square.
             If the square isn't alive:
                 RULE 4:
-                    Bring the square to life
+                    Bring the square to life.
         """
         for coord, square in self.squares.items():
             alive_neighbours = 0
             neighbours = square.neighbours()
 
             for neighbour in neighbours:
-                if self.squares[neighbour].state:
+                if self.squares[neighbour].is_alive:
                     alive_neighbours += 1
 
-            if square.state:
+            if square.is_alive:
                 if alive_neighbours < 2:
-                    square.state = False
+                    square.is_alive = False
                 elif alive_neighbours > 3:
-                    square.state = False
+                    square.is_alive = False
                 else:
                     continue
 
             else:
                 if alive_neighbours == 3:
-                    square.state = True
+                    square.is_alive = True
 
 
 class App:
     """App class: the actual tkinter usage"""
 
-    def __init__(self, length, size, tolerance=0.8):
+    def __init__(self, map_size, side_length, tolerance=0.8):
         """
         Initialization function.
 
-        Will raise exception if the size of the boxes isn't a factor of the window size then,
-        the boxes don't fit evenly.
+        Will raise exception if the side_length of the boxes isn't a factor of the
+        window side_length then, the boxes don't fit evenly.
 
 
         Parameters
         ----------
-        length : int
-            Length of side of window
-        size : int
-            Length of square
+        map_size : int
+            map_size of side of window
+        side_length : int
+            map_size of square
         tolerance : float
             The tolerance of generating alive cells randomly
         """
 
-        self.length = length
-        self.size = size
+        self.map_size = map_size
+        self.side_length = side_length
 
-        if not self.length % self.size == 0:
+        if not self.map_size % self.side_length == 0:
             raise Exception(
                 "The squares don't fit evenly on the screen."
-                + " Box size needs to be a factor of window size."
+                + " Box side_length needs to be a factor of window side_length."
             )
 
         self.grid = Grid(
-            self.length,
-            self.size,
+            self.map_size,
+            self.side_length,
             tolerance,
-            active_col="#008080",
-            inactive_col="white",
+            alive_col="#BAEDCC",
+            dead_col="BLACK",
         )
 
-        self.root = Tk()
-
-        self.canvas = Canvas(self.root, height=self.length, width=self.length)
-
+        self.root = tk.Tk()
+        self.canvas = tk.Canvas(self.root, height=self.map_size, width=self.map_size)
         self.canvas.pack()
-
         self.items = self.update_canvas()
-
         self.root.after(5, self.refresh_screen)
-
         self.root.mainloop()
 
     def refresh_screen(self):
         """Applies the rules to the squares."""
         self.grid.rules()
         self.update_canvas(canvas_done=True, canvas_items=self.items)
-
         self.root.after(5, self.refresh_screen)
 
     def update_canvas(self, canvas_done=False, canvas_items={}):
         """
         If the canvas hasn't already been populated with the .create_rect()
             then Loop through the squares.
-        Draws a rectangle and stores the data in a dict corresponding to the rectangle drawn
+        Draws a rectangle and stores the data in a dict corresponding to the
+        rectangle drawn.
         Need this to update the rectangles' colours later
         If canvas_items has been specified
             Loop through the canvas items
@@ -368,4 +369,4 @@ class App:
 
 
 if __name__ == "__main__":
-    app = App(1000, 25, tolerance=0.5)
+    APP = App(1000, 25, tolerance=0.08)
---
+++ pytest.py
@@ -0,0 +1,4 @@
+#! usr/bin/env python3
+
+from game_of_life import
+import pytest
